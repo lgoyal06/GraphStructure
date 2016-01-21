cefd7d8ce448e8a2dc4753a81011915ffcd54899
Lalit Goyal <lgoyal06@gmail.com>
2016-01-21 20:51:00 +0530

    Added time complexity study for the Add Operation for directed graph

--- src/main/java/com/lalit/directed/graph/operations/GraphOperations.java
+++ src/main/java/com/lalit/directed/graph/operations/GraphOperations.java
@@ -10,30 +10,67 @@ import com.lalit.graph.elements.Edge;
 
 /**
  * @author lalit goyal
+ * @param fromNodeName
+ * @param toNodeName
+ * @param edgeName
+ * @return
  */
 public class GraphOperations {
 
 	Map<String, Set<Edge>> graphMap = new TreeMap<>();
 
-	// TODO : Finish the Logic addOperation
-	// TODO : JUnit test cases
-	// TODO : Time Complexity Study
-	private boolean addOperation(String fromNodeName, String toNodeName, String edgeName) {
+	/**
+	 * <p>
+	 * Time Complexity is 4(logn)+C1+C2+C3+C4. As per the Big-oh notations
+	 * theorem log(n^x) is log(n) So to add one element to the Graph time
+	 * complexity is ~ O(logn)
+	 * </p>
+	 * 
+	 * TODO 1. Finish the Logic addOperation , 2. JUnit test cases
+	 * 
+	 * @param fromNodeName
+	 * @param toNodeName
+	 * @param edgeName
+	 * @return
+	 */
+
+	public boolean addOperation(String fromNodeName, String toNodeName, String edgeName) {
 		try {
+			// O(logn)
 			if (!graphMap.containsKey(fromNodeName)) {
+				// O(logn)
 				graphMap.put(fromNodeName, null);
 			}
+			// CC4-Constant time operations
 			if (toNodeName != null && !toNodeName.equalsIgnoreCase("")) {
+				// Constant time operation - C1
 				Edge edge = new Edge(edgeName, toNodeName);
-				Set<Edge> edgeSet = graphMap.get(fromNodeName) == null ? new HashSet<Edge>()
-						: graphMap.get(fromNodeName);
+				// O(logn)
+				Set<Edge> edgeSet = graphMap.get(fromNodeName);
+				if (edgeSet == null) {
+					// Constant time operation - C2
+					edgeSet = new HashSet<Edge>();
+				}
+				// Constant time operation - C3
 				edgeSet.add(edge);
+				// O(logn)
 				graphMap.put(fromNodeName, edgeSet);
 			}
 			return true;
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	// TODO:Fix it
+	public boolean deleteNodeOperation(String nodeToBeDeleted) {
+		return false;
+
+	}
+
+	// TODO:Fix it
+	public boolean deleteEdgeOperation(String toNodeName, String edgeName) {
+		return false;
 	}
 
 	public static void main(String... s) {
@@ -52,5 +89,4 @@ public class GraphOperations {
 			}
 		}
 	}
-
 }
