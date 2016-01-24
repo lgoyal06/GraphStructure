a1c329fef372e7d8bc9e2f243d8460817183cc15
Lalit Goyal <lgoyal06@gmail.com>
2016-01-24 16:35:06 +0530

    rename the file

---
+++ src/main/java/com/lalit/undirected/graph/operations/UndirectedGraphOperationsImpl.java
@@ -0,0 +1,36 @@
+package com.lalit.undirected.graph.operations;
+
+import com.lalit.graph.operations.GraphOperations;
+
+public class UndirectedGraphOperationsImpl implements GraphOperations {
+
+	@Override
+	public boolean insertOperation(String fromNodeName, String toNodeName, String edgeName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteNodeOperation(String nodeToBeDeleted) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteEdgeOperation(String toNodeName, String edgeName) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateNodeInfo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateEdgeInfo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
